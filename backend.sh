ec500c04056e046c19e5931e1e5535b673524405
udaye <udayeswari.savana@gmail.com>
2023-12-18 21:26:13 -0500

    ecpence app backend

---
+++ backend.sh
@@ -0,0 +1,23 @@
+dnf module disable nodejs -y
+dnf module enable nodejs:18 -y
+
+dnf install nodejs -y
+cp backend.service /etc/systemd/system/backend.service
+
+useradd expense
+
+mkdir /app
+
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
+cd /app
+unzip /tmp/backend.zip
+
+
+npm install
+systemctl daemon-reload
+systemctl enable backend
+systemctl start backend
+
+dnf install mysql -y
+
+mysql -h 172.31.36.97 -uroot -pExpenseApp@1 < /app/schema/backend.sql
